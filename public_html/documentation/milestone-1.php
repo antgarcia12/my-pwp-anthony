97e9f4f8ab6e3be1aee9ef184db6c1ad3bf1ecf5
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-17 22:39:27 -0600

    updated pwp with attitudes, behaviors, technology and other credentials .

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,6 +13,11 @@
 			<p>In the end, my goal is to attract freelance clients, from a wide range of local business, of any type, that could potentially be lasting partnerships.</p>
 		<h1>Persona</h1>
 			<p><strong>Name: </strong>Sandy Sanchez</p>
+			<p><strong>Job:</strong>Nursing, RN</p>
+			<p><strong>Technology: </strong><em>Computer- </em>HP EliteBook x360 1020 Laptop OS: Windows 10 Pro <em>Phone- </em> iPhone X with iOS 12</p>
+			<p><strong>Attitude: </strong>Sandy is a passionate nurse, enjoying what she does everyday, taking care of people on a daily basis. Recently, Sandy had been discussing with her father about a new venture into the house flipping business, as she has the money to do so. Sandy is certainly enthusiastic on what could become of this new challenge.</p>
+			<p><strong>Behavior:</strong>Sandy understand that the task of starting the business is going to be tough, especially trying to do so as nurse, but she loves to multitask. She has the advantage as her father once held a house flipping business when he was younger. One thing that her father wanted to make her aware of was that we live in a very technological era, where everything we look for can be found on the internet. Sandy has the advantage of the internet, as her father didn't have access to such a useful tool when he was younger. It begins to frustrate Sandy because she doesn't know where to begin to promote her business, besides getting in </p>
+			<p></p>
 	</body>
 	</head>
 </html>

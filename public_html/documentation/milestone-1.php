02f2d71fd5ad22fa5d056475ab33050d6d6cdae8
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-17 21:11:58 -0600

    Began the html document of my personal website project

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Website Project</title>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<h3>Purpose: </h3>
+		<p>The purpose of my personal website is to showcase the technologies I've learned at Deep Dive to begin my way into the local business scene that Albuquerque has to offer.</p>
+		<h3>Audience: </h3>
+		<p>The audience that I plan to reach out to would be mainly clients that have local or start up businesses, especially the clients that are trying to put themselves out there any way they can, and in my case that would be a website I create for them.</p>
+		<h3>Goal: </h3>
+		<p></p>
+	</body>
+	</head>
+</html>

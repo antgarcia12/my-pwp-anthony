fd92e8f56cee234feae13fe23b10c64a97cbd482
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-17 22:58:07 -0600

    added content such as the goals and frustrations for the persona portion

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Personal Website Project</title>
+	</head>
 	<body>
 		<h1>Purpose, Audience, Goal</h1>
 		<h3>Purpose: </h3>
@@ -16,8 +17,16 @@
 			<p><strong>Job:</strong>Nursing, RN</p>
 			<p><strong>Technology: </strong><em>Computer- </em>HP EliteBook x360 1020 Laptop OS: Windows 10 Pro <em>Phone- </em> iPhone X with iOS 12</p>
 			<p><strong>Attitude: </strong>Sandy is a passionate nurse, enjoying what she does everyday, taking care of people on a daily basis. Recently, Sandy had been discussing with her father about a new venture into the house flipping business, as she has the money to do so. Sandy is certainly enthusiastic on what could become of this new challenge.</p>
-			<p><strong>Behavior:</strong>Sandy understand that the task of starting the business is going to be tough, especially trying to do so as nurse, but she loves to multitask. She has the advantage as her father once held a house flipping business when he was younger. One thing that her father wanted to make her aware of was that we live in a very technological era, where everything we look for can be found on the internet. Sandy has the advantage of the internet, as her father didn't have access to such a useful tool when he was younger. It begins to frustrate Sandy because she doesn't know where to begin to promote her business, besides getting in </p>
-			<p></p>
+			<p><strong>Behavior:</strong>Sandy understand that the task of starting the business is going to be tough, especially trying to do so as nurse, but she loves to multitask. She has the advantage as her father once held a house flipping business when he was younger. One thing that her father wanted to make her aware of was that we live in a very technological era, where everything we look for can be found on the internet. Sandy has the advantage of the internet, as her father didn't have access to such a useful tool when he was younger. It begins to frustrate Sandy because she doesn't know where to begin to promote her business, besides getting in contact with people. She rather show people that her family has experience in flipping houses rather than explaining over email, phone, or in person with the chance of getting no business, as time is very crucial in her life.</p>
+			<p><strong>Goal: </strong>Sandy wants the goal of creating a website that shows the necessary information for a client to decide whether or not they want her business. She stumbles upon my website and reads that I create websites with the idea of helping local businesses start up or expand on their mission statements.</p>
+		<p><strong>Frustrations: </strong>
+		<ul>
+			<li>Not having enough time for both jobs.</li>
+			<li>A chance of being disorganized.</li>
+			<li>Unable to find houses to flip.</li>
+			<li>Working with a developer who has less than 1 year of experience.</li>
+			<li>Trying to find clients who are looking to flip a house. </li>
+		</ul>
+		</p>
 	</body>
-	</head>
 </html>

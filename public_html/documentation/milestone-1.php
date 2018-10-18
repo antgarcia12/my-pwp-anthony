2d931d13f2270ef5115e27ad73f21e4ccce02c93
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-17 21:44:31 -0600

    updating project with persona including characteristics.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,11 +6,13 @@
 	<body>
 		<h1>Purpose, Audience, Goal</h1>
 		<h3>Purpose: </h3>
-		<p>The purpose of my personal website is to showcase the technologies I've learned at Deep Dive to begin my way into the local business scene that Albuquerque has to offer.</p>
+			<p>The purpose of my personal website is to showcase the technologies I've learned at Deep Dive to begin my way into the local business scene that Albuquerque has to offer.</p>
 		<h3>Audience: </h3>
-		<p>The audience that I plan to reach out to would be mainly clients that have local or start up businesses, especially the clients that are trying to put themselves out there any way they can, and in my case that would be a website I create for them.</p>
+			<p>The audience that I plan to reach out to would be mainly clients that have local or start up businesses, especially the clients that are trying to put themselves out there any way they can, and in my case that would be a website I create for them.</p>
 		<h3>Goal: </h3>
-		<p></p>
+			<p>In the end, my goal is to attract freelance clients, from a wide range of local business, of any type, that could potentially be lasting partnerships.</p>
+		<h1>Persona</h1>
+			<p><strong>Name: </strong>Sandy Sanchez</p>
 	</body>
 	</head>
 </html>

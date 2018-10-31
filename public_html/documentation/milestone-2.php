22722724e985c3f6a7910cde7266d97405bee821
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-30 22:27:29 -0600

    started milestone 2a, adding to the content strategy first

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>My site will be one page, and will consist of the following pages: </p>
+		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a large background image of solid color and display my name and state I am a web developer. Underneath the welcome display, will be four links that will connect the user to my other pages: <em>About Me, Portfolio, Contact.</em></p>
+		<p><strong>About Me: </strong></p>
+	</body>
+</html>

87b9e8381c4da2feec0ef4f2c7f0c9d8fd93444b
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-31 14:06:45 -0600

    updated content strategy, will start working on wireframing.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,6 +10,6 @@
 		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a large background image of solid color and display my name and state I am a web developer. Underneath the welcome display, will be four links that will connect the user to the other parts of my website: <em>About Me, Portfolio, Contact.</em></p>
 		<p><strong>About Me: </strong>My about me section will let the user read about who I am. It will have a condensed version of how I started web developing and the languages I have learned during the bootcamp. I will also include my story of where I am from and some of the interests I have as I want to build some sort of connection with my users. The tone of the whole website will be business-casual as I want to establish that my website is mainly for business, but still have a connection with the client or organizations who like what I have to offer. </p>
 		<p><strong>Portfolio: </strong>This will be the smallest section as it will just simply say "Here is my link to my work I have been working on," with a link to my github profile.</p>
-		<p><strong>Contact: </strong></p>This section of the website will have all my contact information which will include my professional media profiles such as: Github, LinkedIn and my email address.
+		<p><strong>Contact: </strong>This section of the website will have all my contact information which will include my professional media profiles such as: Github, LinkedIn and my email address.</p>
 	</body>
 </html>

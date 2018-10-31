393fc8dfa94b2a71fa6ae59a641b661e2f93c3f1
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-31 14:04:58 -0600

    started milestone 2a, adding to the content strategy first

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8"
+		<meta charset="UTF-8">
 		<title>Milestone 2a</title>
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
-		<p>My site will be one page, and will consist of the following pages: </p>
-		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a large background image of solid color and display my name and state I am a web developer. Underneath the welcome display, will be four links that will connect the user to my other pages: <em>About Me, Portfolio, Contact.</em></p>
-		<p><strong>About Me: </strong>My</p>
+		<p>My site will be one page, and will consist of the following parts: </p>
+		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a large background image of solid color and display my name and state I am a web developer. Underneath the welcome display, will be four links that will connect the user to the other parts of my website: <em>About Me, Portfolio, Contact.</em></p>
+		<p><strong>About Me: </strong>My about me section will let the user read about who I am. It will have a condensed version of how I started web developing and the languages I have learned during the bootcamp. I will also include my story of where I am from and some of the interests I have as I want to build some sort of connection with my users. The tone of the whole website will be business-casual as I want to establish that my website is mainly for business, but still have a connection with the client or organizations who like what I have to offer. </p>
+		<p><strong>Portfolio: </strong>This will be the smallest section as it will just simply say "Here is my link to my work I have been working on," with a link to my github profile.</p>
+		<p><strong>Contact: </strong></p>This section of the website will have all my contact information which will include my professional media profiles such as: Github, LinkedIn and my email address.
 	</body>
 </html>

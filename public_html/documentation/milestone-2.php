2d88bf80061b4cfa8c22a9b7a01f5ad934a6fa11
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-31 22:40:02 -0600

    complete my milestone 2a with the content strategy and the wireframes that show what the mobile version and the desktop version of my pwp

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,9 +7,17 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<p>My site will be one page, and will consist of the following parts: </p>
-		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a large background image of solid color and display my name and state I am a web developer. Underneath the welcome display, will be four links that will connect the user to the other parts of my website: <em>About Me, Portfolio, Contact.</em></p>
+		<p><strong>Welcome: </strong>This section will be the first thing the user will see as it will be overlaid on a solid color background and display my name and state I am a web developer. Underneath my name will be three links that will navigate the user to the other parts of my website: <em>About Me, Portfolio, Contact Me.</em></p>
 		<p><strong>About Me: </strong>My about me section will let the user read about who I am. It will have a condensed version of how I started web developing and the languages I have learned during the bootcamp. I will also include my story of where I am from and some of the interests I have as I want to build some sort of connection with my users. The tone of the whole website will be business-casual as I want to establish that my website is mainly for business, but still have a connection with the client or organizations who like what I have to offer. </p>
-		<p><strong>Portfolio: </strong>This will be the smallest section as it will just simply say "Here is my link to my work I have been working on," with a link to my github profile.</p>
+		<p><strong>Portfolio: </strong>This section will consist of screen shots of my projects that I did in the bootcamp and one sentence simply saying "Here is a link to my projects I have been working on," with a link to my github profile.</p>
 		<p><strong>Contact: </strong>This section of the website will have all my contact information which will include my professional media profiles such as: Github, LinkedIn and my email address.</p>
+		<h1>Wireframes</h1>
+		<h3>Mobile:</h3>
+		<img src="welcome-wireframe.png" alt="Welcome Section">
+		<img src="about-wireframe.png" alt="About Me Section">
+		<img src="portfolio-wireframe.png" alt="Project Section">
+		<img src="contact-wireframe.png" alt="Contact Section">
+		<h3>Desktop:</h3>
+		<img src="web-wireframe.png" alt="Website Look">
 	</body>
 </html>

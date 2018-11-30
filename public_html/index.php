06502f5e83c1799ba99695e5d6c1986ef3072a51
Anthony Garcia <garcia.anthony577@gmail.com>
2018-11-29 22:17:37 -0700

    added cards and portfolio section

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,7 +13,7 @@
 
 
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
@@ -60,19 +60,35 @@
 		</div>
 		</header>
 		<!-- About -->
-		<div class="container-fluid">
-			<div class="row about text-left">
-				<div class="col-lg-6">
-					<h1 class="display-3 abt-me">About Me</h1>
-					<p class="about-me">My name is Anthony Garcia. I am a graduated of the CNM Ingenuity's Deep Diving Coding and earned my certification through the Fullstack Web Development bootcamp they offer. Through my experience of the bootcamp, I learned that many web developers' journeys are all different; it showed me the meaning of diversity in a real work-related environment. I was originally going to school to earn my associates in nursing. Through a process of trial and error, I found that nursing was not the right career for me. I didn't want to waste years trying to find a new career and thankfully, a good friend of mine had mention the bootcamp, which drew my attention to what could come. With research and the help of a "gut feeling," I made the choice to take the first step into the world of coding. In all honesty, I had no prior experience to the bootcamp. The weeks went by and I started to see that I didn't experience going into this. I was fortunate enough to learn years of schooling into 10 of the fastest weeks of my life. I am confident in my capabilities and the product I can produce. My goal is to meet anybody's standards, no matter the challenge, any we can work things out, I hope to change the landscape of our community and economy here in Albuquerque.</p>
-				</div>
-				<div class="col-lg-6 mr-1">
-					<img src="images/dd-coding-icon-cnmii-stem.png" class="img-fluid">
+		<section>
+			<div class="container-fluid about-me">
+				<div class="row about text-left">
+					<div class="col-lg-6">
+						<h1 class="display-3 abt-me">About Me</h1>
+						<p class="story">My name is Anthony Garcia. I am a graduated of the CNM Ingenuity's Deep Diving Coding and earned my certification through the Fullstack Web Development bootcamp they offer. Through my experience of the bootcamp, I learned that many web developers' journeys are all different; it showed me the meaning of diversity in a real work-related environment. I was originally going to school to earn my associates in nursing. Through a process of trial and error, I found that nursing was not the right career for me. I didn't want to waste years trying to find a new career and thankfully, a good friend of mine had mention the bootcamp, which drew my attention to what could come. With research and the help of a "gut feeling," I made the choice to take the first step into the world of coding. In all honesty, I had no prior experience to the bootcamp. The weeks went by and I started to see that I didn't experience going into this. I was fortunate enough to learn years of schooling into 10 of the fastest weeks of my life. I am confident in my capabilities and the product I can produce. My goal is to meet anybody's standards, no matter the challenge, any we can work things out, I hope to change the landscape of our community and economy here in Albuquerque.</p>
+					</div>
+					<div class="col-lg-6 mr-1">
+						<img src="images/dd-coding-icon-cnmii-stem.png" class="dd-fluid">
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 		<!-- Portfolio -->
-
+		<section>
+			<div class="container mt-5 port-work">
+				<div class="row">
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="images/family-photo-stock.jpg" alt="FamilyConn">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">FamilyConnect</h5>
+								<p class="card-text">With the help of my capstone team, I was able to create a family platform that help engage and keep in touch of family, both immediate and distant. Although it may be another social media, this app had a goal to bring families closer and share experiences with loved ones.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 		<!-- Contact -->
 		<form id="contact-form" method="POST" action="php/mailer.php" novalidate="novalidate">
 			<div class="fluid-container main-container vertical-center">
